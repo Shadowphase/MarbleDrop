8af4e181934562d5a9812fbe9f30de8bd9ab7df6
Cody Hall <dragoon364@yahoo.com>
2017-12-02 03:11:25 -0800

    initial commit

---
+++ MarbleDrop/Assets/ballController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballController : MonoBehaviour {
+
+    [SerializeField]
+    int lifespan = 15;
+
+    // Use this for initialization
+    IEnumerator Start () {
+        yield return new WaitForSeconds(lifespan);
+        Destroy(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
