5540d18136b73550ab8dc72b2611e2f307dc92c2
Cody Hall <codthall@gmail.com>
2018-02-01 13:39:51 -0800

    reorganized files

---
+++ Assets/Resources/Scripts/PinBall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinBall : MonoBehaviour {
+	float height;
+	// Use this for initialization
+	void Start () {
+		height = Camera.main.orthographicSize * 2.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//float scale = 2f / (gameObject.transform.position.y + height/2);
+		float scale = 0.3f/((gameObject.transform.position.y / height) + 1);
+		gameObject.transform.localScale = new Vector3(scale, scale, scale);
+	}
+}
